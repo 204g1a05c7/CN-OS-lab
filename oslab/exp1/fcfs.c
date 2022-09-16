039b7853b5a24b95deefe0f1135bd33d66dd1d88
204g1a05c7 <94512754+204g1a05c7@users.noreply.github.com>
2022-09-16 14:52:48 +0530

    Update fcfs.c

--- oslab/exp1/fcfs.c
+++ oslab/exp1/fcfs.c
@@ -2,3 +2,21 @@
 roll no: 204g1a05c7
 name of exp: fcfs algorithm
 */
+#include<stdio.h>
+#include<conio.h>
+main(){
+  int bt[20],wt[20],tat[20],i,n;
+  float wtavg,tatavg;
+  clrscr();
+  printf("\nEnter the number of processes--");
+  scanf("%d",&n);
+  for(i-0;i<i++){
+    printf("\nEnter burst time for process%d--",i);
+    scanf("%d",&bt[i])
+    }
+  wt[0]=wtavg=0;
+  tat[0]=tatavg=bt[0];
+  for(i=1;i<n;i++){
+    wt[i]=wt[i-1]+bt[i-1];
+    
+  
