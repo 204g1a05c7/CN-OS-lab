59e78cdd70cf288738573676678d1472e24953e4
204g1a05c7 <94512754+204g1a05c7@users.noreply.github.com>
2022-09-23 16:16:46 +0530

    Update fcfs.c

--- oslab/exp1/fcfs.c
+++ oslab/exp1/fcfs.c
@@ -1,24 +1,24 @@
-/* name:p gana sree akshaya
-roll no: 204g1a05c7
-name of exp: fcfs algorithm
-*/
 #include<stdio.h>
 #include<conio.h>
-main(){
-  int bt[20],wt[20],tat[20],i,n;
-  float wtavg,tatavg;
-  clrscr();
-  printf("\nEnter the number of processes--");
-  scanf("%d",&n);
-  for(i-0;i<i++){
-    printf("\nEnter burst time for process%d--",i);
-    scanf("%d",&bt[i])
-    }
-  wt[0]=wtavg=0;
-  tat[0]=tatavg=bt[0];
-  for(i=1;i<n;i++){
-    wt[i]=wt[i-1]+bt[i-1];
-    tat[i] = tat[i-1] +bt[i];
+main()
+{
+int bt[20], wt[20], tat[20], i, n;
+float wtavg, tatavg;
+clrscr();
+printf("\nEnter the number of processes -- ");
+scanf("%d", &n);
+for(i=0;i<n;i++)
+{
+printf("\nEnter Burst Time for Process %d -- ", i);
+scanf("%d", &bt[i]);
+}
+wt[0] = wtavg = 0;
+tat[0] = tatavg = bt[0];
+for(i=1;i<n;i++)
+{
+
+wt[i] = wt[i-1] +bt[i-1];
+tat[i] = tat[i-1] +bt[i];
 wtavg = wtavg + wt[i];
 tatavg = tatavg + tat[i];
 }
@@ -29,5 +29,3 @@ printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
 getch();
 }
-    
-  
