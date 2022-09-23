75538c2705950b311942e878c9051f7013c19828
204G1A05c7 <204G1A05c7@srit.ac.in>
2022-09-23 15:30:42 +0530

    first errors compiled

--- oslab/exp1/rr.c
+++ oslab/exp1/rr.c
@@ -1,10 +1,9 @@
 //round robin
 #include<stdio.h>
-main()
+int main()
 {
 int i,j,n,bu[10],wa[10],tat[10],t,ct[10],max;
 float awt=0,att=0,temp=0;
-clrscr();
 printf("Enter the no of processes -- ");
 scanf("%d",&n);
 for(i=0;i<n;i++)
@@ -41,5 +40,4 @@ printf("\nThe Average Waiting time is -- %f ",awt/n);
 printf("\n\tPROCESS\t BURST TIME \t WAITING TIME\tTURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\t%d \t %d \t\t %d \t\t %d \n",i+1,ct[i],wa[i],tat[i]);
-getch();
 }
