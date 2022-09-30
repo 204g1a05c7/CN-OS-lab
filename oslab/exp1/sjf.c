4ce5e5f0645a2f8e1475bfd5e5f66d578fbba23d
204g1a05c7 <94512754+204g1a05c7@users.noreply.github.com>
2022-09-30 14:24:50 +0530

    Update sjf.c

--- oslab/exp1/sjf.c
+++ oslab/exp1/sjf.c
@@ -7,7 +7,6 @@
 {
 int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
 tatavg;
-clrscr();
 printf("\nEnter the number of processes -- ");
 
 scanf("%d", &n);
@@ -40,6 +39,6 @@ printf("\n\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
-printf("\nAverage Turnaround Time -- %f", tatavg/n); getch();}
+printf("\nAverage Turnaround Time -- %f", tatavg/n);}
 
 }
